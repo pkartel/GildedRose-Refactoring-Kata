18aaf15c82018dd7c1f934783bcc4aacd7cbbbc8
Palina Kartsel <kartsel.palina@gmail.com>
2025-03-11 23:21:44 +0100

    extract backstage passes degradation

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,6 +17,16 @@ const minQuality = 0
 
 const degradationStrategies = {
   [GoodsTypes.agedBrie]: (i: Item) => i.quality < maxQuality ? i.quality + (i.sellIn > 0 ? 1 : 2) : i.quality,
+  [GoodsTypes.backstagePass]: (i: Item) => {
+    const { sellIn, quality } = i
+
+    const dif = sellIn <= 0 ? -quality :
+      sellIn < 11
+        ? (sellIn < 6 ? 3 : 2)
+        : 1
+    
+    return quality + dif <= maxQuality ? quality + dif : maxQuality
+  },
 }
 
 export class GildedRose {
@@ -37,41 +47,20 @@ export class GildedRose {
           this.items[i].quality = degradationStrategies[name](this.items[i])
           this.items[i].sellIn = this.items[i].sellIn - 1
           break;
+        case GoodsTypes.backstagePass:
+          this.items[i].quality = degradationStrategies[name](this.items[i])
+          this.items[i].sellIn = this.items[i].sellIn - 1
+          break;
         default:
-          if (this.items[i].name != GoodsTypes.backstagePass) {
-            if (this.items[i].quality > 0) {
-              this.items[i].quality = this.items[i].quality - 1 // default goods
-            }
-          } else { // agedBrie, backstage or sulfuras
-            if (this.items[i].quality < 50) { // agedBrie, backstage
-              this.items[i].quality = this.items[i].quality + 1
-             
-              if (this.items[i].name == GoodsTypes.backstagePass) {
-                if (this.items[i].sellIn < 11) {
-                  if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                  }
-                }
-                if (this.items[i].sellIn < 6) {
-                  if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                  }
-                }
-              }
-            }
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1 // default goods
+          }
+          
+          if (this.items[i].sellIn <= 0 && this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1 // Once the sell by date has passed, Quality degrades twice as fast
           }
 
           this.items[i].sellIn = this.items[i].sellIn - 1;
-
-          if (this.items[i].sellIn < 0) {
-              if (this.items[i].name != GoodsTypes.backstagePass ) {
-                if (this.items[i].quality > 0) {
-                  this.items[i].quality = this.items[i].quality - 1 // Once the sell by date has passed, Quality degrades twice as fast
-                }
-              } else {
-                this.items[i].quality = this.items[i].quality - this.items[i].quality
-              }
-          }
       }
     }
 

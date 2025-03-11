1943537dac66cb0cf6c9d3ec5c5ddfaf8e16a1c8
Palina Kartsel <kartsel.palina@gmail.com>
2025-03-11 20:37:39 +0100

    extract good types, create jest snapshots

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,5 @@
+import { GoodsTypes } from "./types";
+
 export class Item {
   name: string;
   sellIn: number;
@@ -19,16 +21,16 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != GoodsTypes.agedBrie && this.items[i].name != GoodsTypes.backstagePass) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != GoodsTypes.sulfuras) {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name == GoodsTypes.backstagePass) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
@@ -42,14 +44,14 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != GoodsTypes.sulfuras) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != GoodsTypes.agedBrie) {
+          if (this.items[i].name != GoodsTypes.backstagePass) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != GoodsTypes.sulfuras) {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
---
+++ TypeScript/app/types.ts
@@ -0,0 +1,6 @@
+export enum GoodsTypes {
+    agedBrie = 'Aged Brie',
+    backstagePass = 'Backstage passes to a TAFKAL80ETC concert',
+    sulfuras = 'Sulfuras, Hand of Ragnaros',
+    conjured = 'Conjured'
+}
